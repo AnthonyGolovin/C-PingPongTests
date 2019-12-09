0f38daa3b5feeebd136563b1a6293494ed5476aa
Anthony Golovin and Will Quanstrom <anthony+will@>
2019-12-09 10:07:40 -0800

    build C# program that returns the game ping pong to user

---
+++ ping-pong.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number.");
+        string stringReadNumber = Console.ReadLine();
+        int readNumber = int.Parse(stringReadNumber);
+
+        for (int index = 1; index <= readNumber; index++)
+        {
+            if (index % 15 == 0)
+            {
+                Console.WriteLine("ping-pong");
+            }
+            else if (index % 5 == 0)
+            {
+                Console.WriteLine("pong");
+            }
+            else if (index % 3 == 0)
+            {
+                Console.WriteLine("ping");
+            }
+            else
+            {
+                 Console.WriteLine(index);
+            }
+        }
+    }
+}
