209e13f3a16f51348dc3251f692e51addc28f2e2
Anthony Golovingit config --global user.name Anthony <AGOLOVIN97@live.com>
2019-12-15 20:51:38 -0800

    Refactoring for tests

---
+++ PingPong/Models/ping-pong.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number.");
+        string stringReadNumber = Console.ReadLine();
+        int readNumber = int.Parse(stringReadNumber);
+    }
+}
